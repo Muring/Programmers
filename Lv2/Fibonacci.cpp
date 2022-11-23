02d0fbbda2ca80fc2df55be44625b2abf82ae2ca
MuRing <11esh@naver.com>
2022-11-23 23:48:08 +0900

    Vector push_back()

--- Lv2/Fibonacci.cpp
+++ Lv2/Fibonacci.cpp
@@ -7,10 +7,8 @@ int solution(int n) {
     long long answer = 0;
     vector<long long> fibo = { 0, 1 };
 
-    
-    for(int i = 1; i < n; i++){
+    for(int i = 1; i < n; i++)
         fibo.push_back((fibo[i - 1] + fibo[i]) % 1234567);
-    }
     
     answer = fibo.back();
     return answer;

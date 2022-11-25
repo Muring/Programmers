b7418c53dc7eb4930bcfbc250ae76a6f7c0c6a01
MuRing <11esh@naver.com>
2022-11-25 23:37:40 +0900

    LRU Algortihm

---
+++ Lv2/Cache.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    int answer = 0;
+    return answer;
+}
+
+int main(){
+    int cacheSize = 3;
+
+    vector<string> cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju",
+                             "Pangyo", "Seoul", "NewYork", "LA"};
+    solution(cacheSize, cities);
+    return 0;
+}

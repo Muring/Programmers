d5b4d293595b6cf13ecad1a397ce1c3991c0c8b7
MuRing <11esh@naver.com>
2022-11-27 01:02:04 +0900

    LRU Algortihm

--- Lv2/Cache.cpp
+++ Lv2/Cache.cpp
@@ -1,10 +1,35 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 int solution(int cacheSize, vector<string> cities) {
     int answer = 0;
+    vector<string> cache;
+
+    if(cacheSize == 0)
+        return 5 * cities.size();
+
+    for(int i = 0; i < cities.size(); i++){
+        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
+        auto itr = find(cache.begin(), cache.end(), cities[i]);
+
+        // cache miss
+        if(itr == cache.end()){
+            if(cache.size() == cacheSize)
+                cache.erase(cache.begin());
+            answer += 5;
+        }
+        // cache hit
+        else{
+            cache.erase(itr);
+            answer += 1;
+        }
+        cache.push_back(cities[i]);
+    }
+    cout << answer;
     return answer;
 }
 

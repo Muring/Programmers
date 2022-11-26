e9ab8fa5b51f36721228960f75c34ad51a0a1554
MuRing <11esh@naver.com>
2022-11-26 23:53:55 +0900

    Sort

--- Lv2/H_Index.cpp
+++ Lv2/H_Index.cpp
@@ -1,9 +1,29 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
+
+    sort(citations.begin(), citations.end());
+    
+    int count = 0;
+    for(int i = citations.size() - 1; i >= 0; i--){
+        count++;
+        if(citations[i] <= count){
+            answer = citations[i];
+            break;    
+        }
+    }
+    cout << answer;
     return answer;
+}
+
+int main(){
+    vector<int> citations = { 3, 0, 6, 1, 5 };
+    solution(citations);
+    return 0;
 }

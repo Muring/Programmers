cc347dd01abcf2aaaff8ed32828b451b9d9eb2c9
MuRing <11esh@naver.com>
2022-11-29 23:50:18 +0900

    matrix

---
+++ Lv2/matrixMult.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    return answer;
+}
+
+int main(){
+    vector<vector<int>> arr1;
+    vector<vector<int>> arr2;
+    solution(arr1, arr2);
+    return 0;
+}

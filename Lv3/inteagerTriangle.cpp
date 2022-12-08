01177f1be243e2cb0b493f545e748ec00eeb2b44
MuRing <11esh@naver.com>
2022-12-08 23:56:02 +0900

    DP Coding

--- Lv3/inteagerTriangle.cpp
+++ Lv3/inteagerTriangle.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
+
+    
     return answer;
 }
 
 int main(){
-    vector<vector<int>> triangle;
+    vector<vector<int>> triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
     solution(triangle);
     return 0;
 }
